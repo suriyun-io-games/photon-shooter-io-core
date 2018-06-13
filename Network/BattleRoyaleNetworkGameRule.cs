c2073e25b9ca68e8f5fb135d34a96d0cb64aeb4e
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-06-13 08:04:11 +0700

    Make fill bots able to set amount

--- Network/BattleRoyaleNetworkGameRule.cs
+++ Network/BattleRoyaleNetworkGameRule.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class BattleRoyaleNetworkGameRule : IONetworkGameRule
 {
-    public bool fillBots;
+    [Tooltip("Maximum amount of bots will be filled when start game")]
+    public int fillBots = 10;
     public int endMatchCountDown = 10;
     public int EndMatchCountingDown { get; protected set; }
     public override bool HasOptionBotCount { get { return false; } }
@@ -101,10 +102,12 @@ public class BattleRoyaleNetworkGameRule : IONetworkGameRule
 
     public override void AddBots()
     {
-        if (!fillBots)
+        if (fillBots <= 0)
             return;
 
         var botCount = networkManager.maxConnections - networkManager.Characters.Count;
+        if (botCount > fillBots)
+            botCount = fillBots;
         for (var i = 0; i < botCount; ++i)
         {
             var character = NewBot();
