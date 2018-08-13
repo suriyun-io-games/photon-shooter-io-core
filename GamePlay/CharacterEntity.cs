6f961b58f5fd725125b0fff618fbf71ba06dd8a7
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-08-14 02:26:32 +0700

    Improve input movement

--- GamePlay/CharacterEntity.cs
+++ GamePlay/CharacterEntity.cs
@@ -696,11 +696,11 @@ public class CharacterEntity : BaseNetworkGameCharacter
         inputMove = Vector2.zero;
         inputDirection = Vector2.zero;
         inputAttack = false;
-        inputJump = false;
         if (canControl)
         {
             inputMove = new Vector2(InputManager.GetAxis("Horizontal", false), InputManager.GetAxis("Vertical", false));
-            inputJump = InputManager.GetButtonDown("Jump");
+            if (!inputJump)
+                inputJump = InputManager.GetButtonDown("Jump") && isGround;
             if (isMobileInput)
             {
                 inputDirection = new Vector2(InputManager.GetAxis("Mouse X", false), InputManager.GetAxis("Mouse Y", false));
@@ -746,7 +746,7 @@ public class CharacterEntity : BaseNetworkGameCharacter
             TempRigidbody.AddForce(velocityChange, ForceMode.VelocityChange);
         }
     }
-    
+
     protected virtual void UpdateMovements()
     {
         if (!photonView.isMine || Hp <= 0)
@@ -765,12 +765,20 @@ public class CharacterEntity : BaseNetworkGameCharacter
         {
             TempRigidbody.velocity = new Vector3(velocity.x, CalculateJumpVerticalSpeed(), velocity.z);
             isGround = false;
+            inputJump = false;
         }
+    }
+    
+    protected virtual void OnCollisionEnter(Collision collision)
+    {
+        if (!isGround && collision.impulse.y > 0)
+            isGround = true;
     }
 
     protected virtual void OnCollisionStay(Collision collision)
     {
-        isGround = true;
+        if (!isGround && collision.impulse.y > 0)
+            isGround = true;
     }
 
     protected float CalculateJumpVerticalSpeed()
@@ -789,7 +797,7 @@ public class CharacterEntity : BaseNetworkGameCharacter
             TempTransform.rotation = targetRotation;
         }
     }
-    
+
     public void GetDamageLaunchTransform(bool isLeftHandWeapon, out Transform launchTransform)
     {
         launchTransform = null;
