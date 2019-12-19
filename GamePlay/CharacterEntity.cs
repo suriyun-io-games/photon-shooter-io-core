1fa92fbe4d42f88ada509b7043505de211681f4a
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-12-19 11:26:02 +0700

    Improve battle royale

--- GamePlay/CharacterEntity.cs
+++ GamePlay/CharacterEntity.cs
@@ -375,9 +375,6 @@ public class CharacterEntity : BaseNetworkGameCharacter
         get { return isHidding; }
         set
         {
-            if (isHidding == value)
-                return;
-
             isHidding = value;
             var renderers = GetComponentsInChildren<Renderer>();
             foreach (var renderer in renderers)
