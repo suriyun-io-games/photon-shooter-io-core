071bc62f864d29f1fc272252b058d099d417a432
Insthync <ittipon.bay@gmail.com>
2021-07-15 01:09:00 +0700

    Add `returnToMoveDirectionDelay` to `CharacterEntity`

--- GamePlay/CharacterEntity.cs
+++ GamePlay/CharacterEntity.cs
@@ -45,6 +45,7 @@ public class CharacterEntity : BaseNetworkGameCharacter
     public GameObject[] localPlayerObjects;
     public float dashDuration = 1.5f;
     public float dashMoveSpeedMultiplier = 1.5f;
+    public float returnToMoveDirectionDelay = 1f;
     [Header("UI")]
     public Transform hpBarContainer;
     public Image hpFillImage;
@@ -58,9 +59,9 @@ public class CharacterEntity : BaseNetworkGameCharacter
     public GameObject attackSignalObjectForTeamB;
     [Header("Effect")]
     public GameObject invincibleEffect;
-    [Header("Online data")]
 
     #region Sync Vars
+    [Header("Online data")]
     protected int _hp;
     protected int _armor;
     protected int _exp;
@@ -372,6 +373,7 @@ public class CharacterEntity : BaseNetworkGameCharacter
     protected float dashingTime;
     protected Vector3? previousPosition;
     protected Vector3 currentVelocity;
+    protected float lastActionTime;
 
     public float startReloadTime { get; private set; }
     public float reloadDuration { get; private set; }
@@ -805,6 +807,8 @@ public class CharacterEntity : BaseNetworkGameCharacter
             if (canAttack)
                 inputAttack = InputManager.GetButton("Fire1");
         }
+        if (inputAttack)
+            lastActionTime = Time.unscaledTime;
     }
 
     protected virtual void UpdateViewMode(bool force = false)
@@ -1012,7 +1016,7 @@ public class CharacterEntity : BaseNetworkGameCharacter
         // Turn character to move direction
         if (inputDirection.magnitude <= 0 && inputMove.magnitude > 0 || viewMode == ViewMode.ThirdPerson)
             inputDirection = inputMove;
-        if (characterModel && characterModel.TempAnimator && characterModel.TempAnimator.GetBool("DoAction") && viewMode == ViewMode.ThirdPerson)
+        if (characterModel && characterModel.TempAnimator && (characterModel.TempAnimator.GetBool("DoAction") || Time.unscaledTime - lastActionTime <= returnToMoveDirectionDelay) && viewMode == ViewMode.ThirdPerson)
             inputDirection = cameraForward;
         if (!IsDead)
             Rotate(isDashing ? dashInputMove : inputDirection);
